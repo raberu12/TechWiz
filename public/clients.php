8e07865534d8e393d06b5af79c17b5d96ef9a182
raberu12 <11400071@usc.edu.ph>
2023-05-24 21:49:33 +0800

    Update clients.php

--- public/clients.php
+++ public/clients.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../src/css/client.css">
+    <link rel="stylesheet" type="text/css" href="../src/css/home.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>TechWiz</title>
 </head>
