4d7b38f636d7a155d318821661306071b8beaff4
raberu12 <11400071@usc.edu.ph>
2023-05-25 15:15:01 +0800

    css changes

--- public/register.php
+++ public/register.php
@@ -16,7 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sign up</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../src/css/signup.css">
+    <link rel="stylesheet" type="text/css" href="../src/css/register.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
