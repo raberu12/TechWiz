bdd29791a3120083aa69366f941688708de10bc1
raberu12 <11400071@usc.edu.ph>
2023-05-25 16:10:30 +0800

    added no clients option

--- public/clients.php
+++ public/clients.php
@@ -1,6 +1,28 @@
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "techwizdb";
+
+$connection = mysqli_connect($servername, $user, $password, $dbname);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Read all rows from the database table
+$sql = "SELECT * FROM clients";
+$result = $connection->query($sql);
+
+if (!$result) {
+    die("Invalid query: " . $connection->error);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Head content -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +32,7 @@
     <title>TechWiz</title>
 </head>
 <body>
-<div class="main_nav">
+    <div class="main_nav">
         <div class="title">
             <h1>TechWiz</h1><img src="../src/img/techwizlg.png">
         </div>
@@ -35,71 +57,61 @@
         </div>
     </div>
     <div class="container my-5">
-        <h2>List of Clients</h2>
-        <a class="btn btn-primary" href="../src/php/create.php" role="button">New Client</a>
-        <br>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                    <th>Created At</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                
-                    $servername = "localhost";    
-                    $user = "root";        
-                    $password = "";         
-                    $dbname = "techwizdb"; 
-                
-                    $connection = mysqli_connect($servername, $user, $password, $dbname);
-                
-                    if (!$connection) {
-                        die("Connection failed: " . mysqli_connect_error());
-                    }
-
-                    //read all row from database table
-                    $sql = "SELECT * FROM clients";
-                    $result =$connection->query($sql);
-
-                    if(!$result){
-                        die("Invalud query: " . $connection->error);
-                    }
-
-                    //read data of each row
-                    while($row = $result->fetch_assoc()){
-                        echo "
-                        <tr>
-                        <td>$row[id]</td>
-                        <td>$row[name]</td>
-                        <td>$row[email]</td>
-                        <td>$row[phone]</td>
-                        <td>$row[address]</td>
-                        <td>$row[created_at]</td>
-                        <td>
-                            <a class='btn btn-primary' href='../src/php/edit.php?id=$row[id]'>Update</a>
-                            <a class='btn btn-danger' href='../src/php/delete.php?id=$row[id]'>Delete</a>
-                        </td>
+        <?php
+        // Check if there are no clients in the list
+        if ($result->num_rows === 0) {
+            echo '
+            <h2 class="text-center">Currently no clients in the list!</h2>
+            <div class="text-center">
+                <a class="btn btn-primary" href="../src/php/create.php" role="button">New Client</a>
+            </div>';
+        } else {
+            echo '
+            <h2>List of Clients</h2>
+            <a class="btn btn-primary" href="../src/php/create.php" role="button">New Client</a>
+            <br>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Created At</th>
+                        <th>Action</th>
                     </tr>
-                    ";
-                    }
+                </thead>
+                <tbody>';
 
+            // Read data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo '
+                <tr>
+                    <td>' . $row["id"] . '</td>
+                    <td>' . $row["name"] . '</td>
+                    <td>' . $row["email"] . '</td>
+                    <td>' . $row["phone"] . '</td>
+                    <td>' . $row["address"] . '</td>
+                    <td>' . $row["created_at"] . '</td>
+                    <td>
+                        <a class="btn btn-primary" href="../src/php/edit.php?id=' . $row["id"] . '">Update</a>
+                        <a class="btn btn-danger" href="../src/php/delete.php?id=' . $row["id"] . '">Delete</a>
+                    </td>
+                </tr>';
+            }
 
-                ?>        
-                
-            </tbody>
-        </table>
+            echo '
+                </tbody>
+            </table>';
+        }
+        ?>
     </div>
     <footer>
         <div class="footer-text">
-            &copy;  TechWiz All Rights Reserved
+            &copy; TechWiz All Rights Reserved
+        </div>
     </footer>
-    <script type="text/javascript" src="../src/js/authenticate.js"></script> 
+    <script type="text/javascript" src="../src/js/authenticate.js"></script>
 </body>
 </html>
